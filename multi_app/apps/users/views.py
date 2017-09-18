41e629c7539880225628b1d7ca4142f3441b47c4
Lex Myers <lex.myers@gmail.com>
2017-09-18 13:01:32 -0700

    finished user routes

--- multi_app/apps/users/views.py
+++ multi_app/apps/users/views.py
@@ -3,6 +3,14 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
 
-def index(request):
-    response = "This is for the /users route"
-    return HttpResponse(response)
+def index(request, type):
+    print type
+    if type == 'users':
+        response = "placeholder to later display all the list of users"
+        return HttpResponse(response)
+    elif type == 'register' or type == 'new':
+        response = "placeholder for users to create a new user record"
+        return HttpResponse(response)
+    elif type == 'login':
+        response = "placeholder for users to login"
+        return HttpResponse(response)
