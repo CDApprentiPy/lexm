b0011c17649e461e2f07f1fb3ca7b269b84c54f2
Lex Myers <lex.myers@gmail.com>
2017-09-18 12:01:04 -0700

    added surveys

--- multi_app/apps/surveys/views.py
+++ multi_app/apps/surveys/views.py
@@ -4,5 +4,9 @@ from __future__ import unicode_literals
 from django.shortcuts import render, HttpResponse, redirect
 
 def index(request):
-    response = "This is for the /surveys route"
+    response = "placeholder to display all the surveys created"
+    return HttpResponse(response)
+
+def new(request):
+    response = "placeholder for users to add a new survey"
     return HttpResponse(response)
