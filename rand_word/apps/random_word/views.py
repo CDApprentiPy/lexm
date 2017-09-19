2240d399406002b22cd50b6d5360b4a347db1af4
Lex Myers <lex.myers@gmail.com>
2017-09-18 17:01:44 -0700

    added random word

--- rand_word/apps/random_word/views.py
+++ rand_word/apps/random_word/views.py
@@ -2,15 +2,18 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render, HttpResponse, redirect
+from django.utils.crypto import get_random_string
+
 
 def index(request):
     if not 'count' in request.session:
         request.session['count'] = 1
     else:
         request.session['count'] += 1
+    randword = get_random_string(length=14)
     context = {
         "attempt" : request.session['count'],
-        "randword" : "not a word"
+        "randword" : randword,
     }
     return render(request, "random_word/index.html", context)
 
