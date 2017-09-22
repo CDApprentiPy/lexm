74de0a013a629d71a8f80e47d2a428c87f406b8f
Lex Myers <lex.myers@gmail.com>
2017-09-22 16:36:56 -0700

    changes to trial tests

--- user_dashboard/apps/user_dash/tests.py
+++ user_dashboard/apps/user_dash/tests.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from django.urls import resolve
 from django.test import TestCase
+from .views import index
 
-class FailTest(TestCase):
+# class FailTest(TestCase):
+#
+#     def test_something_bad(self):
+#         self.assertEqual(1 + 1, 3)
 
-    def test_something_bad(self):
-        self.assertEqual(1 + 1, 3)
+class HomePageTest(TestCase):
+    def test_root_url_resolves(self):
+        found = resolve('/')
+        self.assertEqual(found.func, index)
