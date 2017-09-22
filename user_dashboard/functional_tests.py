15affa40549f398ee24fd71ad55faaf0a71d5980
Lex Myers <lex.myers@gmail.com>
2017-09-22 15:31:53 -0700

    updated test

--- user_dashboard/functional_tests.py
+++ user_dashboard/functional_tests.py
@@ -1,6 +1,19 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Firefox()
-browser.get('http://localhost:8000')
+class NewTest(unittest.TestCase):
 
-assert 'Home' in browser.title
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_if_page_loads(self):
+        self.browser.get('http://localhost:8000')
+        print self.browser.title
+        self.assertIn('Home', self.browser.title)
+        self.fail('Not loading!!!')
+
+if __name__ == '__main__':
+    unittest.main()
